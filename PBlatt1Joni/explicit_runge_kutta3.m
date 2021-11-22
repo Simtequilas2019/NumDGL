f5601879a1bf8f92b004f6470716fe4b3ea54303
Jonathan Schnitzler <jonathanschnitzler@web.de>
2021-11-22 11:33:51 +0100

    Meins ist besser!

--- PBlatt1Joni/explicit_runge_kutta3.m
+++ PBlatt1Joni/explicit_runge_kutta3.m
@@ -13,13 +13,30 @@ yRK(:,1) = y_0;
 %     a = [0 0 0;  1/2 0 0;  -1 2 0];
 %     b = [1/6 2/3 1/6];
 %     c = [0 1/2 1];
+% for k = 1:K
+%     v1 = yRK(:,k);
+%     v2 = yRK(:,k) + tau * 1/2 * f(param, t(k), v1);
+%     v3 = yRK(:,k) + tau * (-1 * f(param,t(k),v1) + 2 * f(param,t(k)+ 1/2 * tau, v2));
+%     sumV = 1/6 * f(param,t(k), v1) + 2/3 * f(param, t(k) + tau/2, v2);
+%     sumV = sumV + 1/6 * f(param, t(k) + tau, v3);
+%     yRK(:,k + 1) = yRK(:,k) + tau * sumV;
+% end
+% 
+% end
+% 
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Oder alternativ mit effizienterem Speichern der Einträge %
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% 
+% Dies spart drei Funktionsaufrufe und damit halbiert es die Anzahl der
+% Funktionsaufrufe, was je nach Funktion, sehr vorteilhaft sein kann
 for k = 1:K
     v1 = yRK(:,k);
-    v2 = yRK(:,k) + tau * 1/2 * f(param, t(k), v1);
-    v3 = yRK(:,k) + tau * (-1 * f(param,t(k),v1) + 2 * f(param,t(k)+ 1/2 * tau, v2));
-    sumV = 1/6 * f(param,t(k), v1) + 2/3 * f(param, t(k) + tau/2, v2);
+    dummy1 = f(param, t(k), v1);
+    v2 = yRK(:,k) + tau * 1/2 * dummy1;
+    dummy2 = f(param,t(k)+ 1/2 * tau, v2);
+    v3 = yRK(:,k) + tau * (-1 * dummy1 + 2 * dummy2);
+    sumV = 1/6 * dummy1 + 2/3 * dummy2;
     sumV = sumV + 1/6 * f(param, t(k) + tau, v3);
     yRK(:,k + 1) = yRK(:,k) + tau * sumV;
-end
-
 end
