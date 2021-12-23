ae99a1668298f2bc98888750ad0774ffd008e10f
Jonathan Schnitzler <jonathanschnitzler@web.de>
2021-12-23 10:15:30 +0100

    gg

--- PBlatt2/test.m
+++ PBlatt2/test.m
@@ -1,17 +1,6 @@
-omeg.o = [0,0];
-omeg.x = 3;
-omeg.y = 3;
-omeg.h = 0.1;
-O1 = genRectMesh2D(omeg);
-omeg.x = 2;
-omeg.y = 2;
-omeg.o = [1.3, 1.5];
-O2 = genRectMesh2D(omeg);
-OM1 = O1.pointMat
-OM2 = O2.pointMat
-plot(OM1(:,1), OM1(:,2),'ro'); hold on
-plot(OM2(:,1), OM2(:,2),'b*'); hold off
+x = linspace(1,100,1000);
+y = x.^2 - 1/1000 * x.^3;
 
-pl = OM1(~logical(isInO2),:)
-figure
-plot(pl(:,1),pl(:,2),'ro');
+plot(x,y)
+xlim([0,10])
+ylim([0,10])
